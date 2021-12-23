efe9fe67b6082ac7c383825a8bf576fef48bef66
unknown <derek.slauson5@gmail.com>
2021-12-23 09:59:25 -0600

    Added error handeling to find all books route

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,9 +1,13 @@
 const router = require("express").Router()
 const Book = require("../models/book")
 
-router.get("/", async (req, res) => {
-    const foundBooks = await Book.find()
-    res.json(foundBooks)
+router.get("/", async (_req, res) => {
+    try {
+        const foundBooks = await Book.find()
+        res.status(200).json(foundBooks)
+    } catch(error) {
+        res.status(404).json({message: `ERROR: ${error}`})
+    }
 })
 
 module.exports = router
